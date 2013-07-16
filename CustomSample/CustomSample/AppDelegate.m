78a0780b8fd1bdb00efd5dfc60101aca1c3b2d42
Grantland Chew <grantlandchew@gmail.com>
2013-07-16 16:53:45 -0700

    Re-add didStartSessionWithPoptart callback

--- CustomSample/CustomSample/AppDelegate.m
+++ CustomSample/CustomSample/AppDelegate.m
@@ -28,6 +28,7 @@
     
     // Instantiate and set the shared Kiip instance.
     Kiip *kiip = [[Kiip alloc] initWithAppKey:KP_APP_KEY andSecret:KP_APP_SECRET];
+    kiip.delegate = self;
     // Usually the notificationView is set here, but for the sake of this sample it is called in
     // toggleCustomNotification.
     // kiip.notificationView = self.customNotificationView;
@@ -36,7 +37,7 @@
     return YES;
 }
 
--(void)toggleCustomNotification:(BOOL)enabled {
+- (void)toggleCustomNotification:(BOOL)enabled {
     // Usually this would be set right after Kiip.init, but since this
     // sample allows enabling/disabling, we have it here.
     [[Kiip sharedInstance] setNotificationView:enabled ? self.customNotificationView : nil];
@@ -45,6 +46,20 @@
 - (void)showError:(NSError *)error {
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:[error localizedDescription] delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
     [alert show];
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark KiipDelegate
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+- (void)kiip:(Kiip *)kiip didStartSessionWithPoptart:(KPPoptart *)poptart error:(NSError *)error {
+    if (error) {
+        [self showError:error];
+    }
+    if (poptart) {
+        [poptart show];
+    }
 }
 
 @end
